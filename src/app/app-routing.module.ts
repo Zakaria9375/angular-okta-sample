c8326209d86015e35dd19489966d1effde2caf1e
Zakaria Ali <alizakaria9375@gmail.com>
2024-05-19 17:58:21 +0300

    deploy(test): core functionality

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import {
+  Routes,
+} from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ExternalApiComponent } from './pages/external-api/external-api.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { authGuardFn } from '@auth0/auth0-angular';
+import {CallbackComponent} from "./pages/callback/callback.component";
+
+export const routes: Routes = [
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [authGuardFn],
+  },
+  {
+    path: 'external-api',
+    component: ExternalApiComponent,
+    canActivate: [authGuardFn],
+  },
+  {
+    path: 'authorize',
+    component: CallbackComponent,
+  },
+  {
+    path: 'error',
+    component: ErrorComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+];
---
+++ src/app/pages/callback/callback.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-callback',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './callback.component.html',
+  styleUrl: './callback.component.css'
+})
+export class CallbackComponent {
+
+}
